5ff8961ebc97ee99d867ec641a20f3236934b68d
Slava Tamashchuk <slava.tamashchuk@gmail.com>
2024-03-01 13:09:13 +0200

    Defs praperad

---
+++ python_files/money_system.py
@@ -0,0 +1,14 @@
+def save_changes_to_json():
+    print("Hello World!")
+
+def give_money(platform_type, giver, receiver, money_amount):
+    print("Hello World!")
+
+def show_balace(person):
+    print("Hello World!")
+
+def show_general_money_amount():
+    print("Hello World!")
+
+def show_inflation_rate():
+    print()
